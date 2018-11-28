6ea9b011772be24ac0f1fa7ad52ea07576404b3a
Marcy Silverman <marcysilve225@gmail.com>
2018-11-28 15:25:31 -0800

    info rendered

--- src/components/Ipsum.vue
+++ src/components/Ipsum.vue
@@ -1,6 +1,13 @@
 <template>
     <li>
         <h3>{{ipsum.title}}</h3>
+        <div class="info">
+            <h3>{{ipsum.author}}</h3>
+            <p>{{ipsum.category}}</p>
+            <p>{{ipsum.authorURL}}</p>
+            <p>{{ipsum.publishedOn}}</p>
+            <p>{{ipsum.body}}</p>
+        </div>
     </li>
 </template>
 
